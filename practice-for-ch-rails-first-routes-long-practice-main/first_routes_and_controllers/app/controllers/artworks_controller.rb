f66020f888c588476d65b400293e76019a02353f
Shuhei Shibahara <Shoehey06@gmail.com>
2022-09-14 17:13:15 -0700

    finished artwork controller

--- practice-for-ch-rails-first-routes-long-practice-main/first_routes_and_controllers/app/controllers/artworks_controller.rb
+++ practice-for-ch-rails-first-routes-long-practice-main/first_routes_and_controllers/app/controllers/artworks_controller.rb
@@ -1,5 +1,38 @@
 class ArtworksController < ApplicationController
 
+    def create
+
+        @artwork = Artwork.new(artwork_params)
+        if @artwork.save
+            redirect_to artwork_url(@artwork), status: :created
+        else
+            render json: @artwork.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        Artwork.destroy_by(id: params(:id)) render json: ['Burned artwork']
+    end
+
+    def index
+        @artwork = Artwork.all
+        render json: @artwork
+    end
+
+    def show
+        @artwork = Artwork.find_by(id: params(:id))
+        render json: @artwork
+    end
+
+    def update
+        @artwork = Artwork.find_by(id: params(:id))
+        
+        if @artwork.update(artwork_params)
+            redirect_to artwork_url(@artwork), status: :updated
+        else
+            render json: @artwork.errors.full_messages, status: :unprocessable_entity
+        end
+    end
 
 
 
@@ -8,6 +41,6 @@ class ArtworksController < ApplicationController
     private 
 
     def artwork_params 
-        param.require(:artwork).permit(:)
+        param.require(:artwork).permit(:title, :image_url, :artist_id)
     end 
 end
